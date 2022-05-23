e3561d7587279a1326533b3867a36018936ba3ef
Alex Marcelo <qualityprint.atendimento@gmail.com>
2022-05-23 16:26:58 -0300

    Adicionado a cor vermelha no relatório de vendas simples

---
+++ SalesWebMvc/Migrations/20220520200654_AlteracaoDatasDados.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class AlteracaoDatasDados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Vendedor",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Vendedor",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Vendedor",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Vendedor",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
